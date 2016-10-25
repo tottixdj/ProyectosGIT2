bbf22bd00d37814e0e62c4aea505b3907de8c3af
Jesús García Galiana <Jess Garca Galiana>
2016-10-26 01:24:20 +0200

    Creada clase suma

---
+++ sumar.java
@@ -0,0 +1,12 @@
+
+public class sumar {
+
+		public static void main (String [] args){
+			
+			int a=40;
+			int b=30;
+			int suma=a+b;
+			
+			System.out.println("suma de "+a+" y "+b+" es: "+suma);
+		}
+}
