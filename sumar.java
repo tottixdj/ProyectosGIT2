50c1f77a20748abaa8751d2f1b6f5b5a7d103722
Jesús García Galiana <jggtotti@gmail.com>
2016-10-26 01:42:51 +0200

    añadida la resta

--- sumar.java
+++ sumar.java
@@ -6,7 +6,8 @@ public class sumar {
 			int a=40;
 			int b=30;
 			int suma=a+b;
-			
+			int resta=a-b;
 			System.out.println("suma de "+a+" y "+b+" es: "+suma);
+			System.out.println("resta de "+a+" y "+b+" es: "+resta);
 		}
 }
